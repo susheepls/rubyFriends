47ee546e213e130e6d508edf2f29ef38a33e393c
susheepls <169642127+susheepls@users.noreply.github.com>
2024-08-19 20:10:05 +0900

    Feat: add create path for users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,30 @@
 class UsersController < ApplicationController
   def index
     @users = User.all
-    render json: @users
+    # render json: @users
   end
 
   def show
     @user = User.find(params[:id])
     render json: @user
   end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new( user_params )
+    if @user.save
+      redirect_to @user
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+    def user_params
+      params.require(:user).permit(:username, :password, :profile_name, :description)
+    end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+  validates :username, presence: true, length: { minimum:3 }
+  validates :password, presence: true, length: {minimum:5}
+  validates :profile_name, presence: true, length: {minimum:3}
+  validates :description, presence: true, length: {minimum:1}
 end
