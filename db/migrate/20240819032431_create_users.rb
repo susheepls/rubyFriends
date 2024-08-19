7a6f962be941018441dd2794b6e2123de7356233
susheepls <169642127+susheepls@users.noreply.github.com>
2024-08-19 16:48:20 +0900

    Feat: create User table and MVC; add routes for all users and by id

--- db/migrate/20240819032431_create_users.rb
+++ db/migrate/20240819032431_create_users.rb
@@ -1,9 +1,10 @@
 class CreateUsers < ActiveRecord::Migration[7.2]
   def change
     create_table :users do |t|
-      t.string :username
+      t.string :username, index: {:unique => true}
       t.string :password
-      
+      t.string :profile_name, index: {:unique => true}
+      t.text :description
       t.timestamps
     end
   end
