0027a98b66a08b04bac7ac08b38e81fd3d56098f
susheepls <169642127+susheepls@users.noreply.github.com>
2024-08-20 11:30:37 +0900

    Feat: finish basic CRUD functionality for users
    The User cruds are all done but I have to make edits later since the css on the page is terrible. I will also have to use these as foreign keys laters.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,6 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    render json: @user
   end
 
   def new
@@ -14,12 +13,31 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new( user_params )
+    @user = User.new(user_params)
     if @user.save
       redirect_to @user
     else
       render :new, status: :unprocessable_entity
     end
+  end
+
+  def edit
+    @user = User.find(params[:id])  
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to @user
+    else render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+
+    redirect_to root_path, status: :see_other
   end
 
   private
